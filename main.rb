60d7bb52daff0c13857910cc45c57936d3a2891d
Ildar Semenistyy <IldarSemenistyy@gmail.com>
2014-07-09 16:51:10 +0600

    Changed project structure

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 # Точка входа в приложение
 
 # Импортируем связаные классы
-require_relative 'person'
-require_relative 'jury'
-require_relative 'challenger'
-require_relative 'casting'
-require_relative 'act'
-require_relative 'role'
+require_relative 'classes/person'
+require_relative 'classes/jury'
+require_relative 'classes/challenger'
+require_relative 'classes/casting'
+require_relative 'classes/act'
+require_relative 'classes/role'
 
 # Задаем роли
 baba_yaga = Role.new(:woman, 50)
